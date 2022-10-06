cff3629433225cea3e207a4aaba521ee6ce257a9
Celso França Neto <celso.franca.neto@gmail.com>
2022-10-06 17:44:32 -0300

    non-responsive menu bar

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function showMenuProfile () {
+    var menuProfile = document.getElementById("menu-profile");
+    if(menuProfile.style.display == "none") {
+        menuProfile.style.display = "flex";
+    }else {
+        menuProfile.style.display = "none";
+    }
+
+}
