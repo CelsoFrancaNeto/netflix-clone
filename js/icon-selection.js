d5731ecb08122a971722d7e1a7ead7838cf3aebd
CelsoFrancaNeto <celso.franca.neto@gmail.com>
2022-10-19 22:00:33 -0300

    avatar-icon-v2

--- js/icon-selection.js
+++ js/icon-selection.js
@@ -1,12 +1,30 @@
+function selectAvatar(elemento) {
+   var avatars = document.getElementsByClassName("profile-img")
 
-function selectIcon(elemento) {
-   if(elemento.style.border != "solid red") {
-      //document.getElementsByClassName("profile-img").style.border = "none";
+   if (elemento.style.border == "solid red") {
 
-
-      elemento.style.border = "solid red";
-
-   }else {
       elemento.style.border = "none";
+
+   } else {
+
+      for (i = 0; i < avatars.length; i++) {
+         avatars[i].style.border = "none"
+      }
+      elemento.style.border = "solid red";
    }
+}
+
+function submitAvatar() {
+
+   
+   var icon = document.getElementById("icon-profile");
+   console.log(icon)
+   var avatars = document.getElementsByClassName("profile-img")
+
+   for(i = 0; i < avatars.length ; i++)
+      if(avatars[i].style.border == "solid red")
+         icon.src= "img/profile03.png";
+
+   
+
 }
